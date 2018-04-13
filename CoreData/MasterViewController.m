6b66b35dfeffc4dedd57335a2abfbaaf7485e5b6
Devepre <bicycle2life@gmail.com>
2018-04-13 14:22:28 +0300

    right bar button moved to storyboard

--- CoreData/MasterViewController.m
+++ CoreData/MasterViewController.m
@@ -18,20 +18,8 @@
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-    
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
-    
     self.managedObjectContext = [DataController sharedInstance].managedObjectContext;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Additional Methods
@@ -56,6 +44,12 @@
     //            NSAssert(NO, @"Error saving context: %@\n%@", [error localizedDescription], error.userInfo);
     //        }
     //    }
+}
+
+#pragma mark - Actions
+
+- (IBAction)addItemAction:(UIBarButtonItem *)sender {
+    [self insertNewObject:nil];
 }
 
 #pragma mark - Table view data source
